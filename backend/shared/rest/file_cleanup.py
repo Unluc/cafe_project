17be85b042acdd31b2a678d0a95d8319d5034e2d
Unluc <padalka9619@gmail.com>
2023-07-08 14:43:06 +0100

    When creating container db take dump data

--- backend/shared/rest/file_cleanup.py
+++ backend/shared/rest/file_cleanup.py
@@ -5,39 +5,7 @@ from django.db.models import FileField
 from django.db.models.signals import pre_save, post_delete
 from django.dispatch import receiver
 
-# def file_cleanup(sender, **kwargs):
-#     """
-#     File cleanup callback used to emulate the old delete
-#     behavior using signals. Initially django deleted linked
-#     files when an object containing a File/ImageField was deleted.
 
-#     Usage:
-#     >>> from django.db.models.signals import post_delete
-#     >>> post_delete.connect(file_cleanup, sender=MyModel, dispatch_uid="mymodel.file_cleanup")
-#     """
-#     for fieldname in sender._meta.get_all_field_names():
-#         try:
-#             field = sender._meta.get_field(fieldname)
-#         except:
-#             field = None
-
-#     if field and isinstance(field, FileField):
-#         inst = kwargs["instance"]
-#         f = getattr(inst, fieldname)
-#         m = inst.__class__._default_manager
-#         if (
-#             hasattr(f, "path")
-#             and os.path.exists(f.path)
-#             and not m.filter(
-#                 **{"%s__exact" % fieldname: getattr(inst, fieldname)}
-#             ).exclude(pk=inst._get_pk_val())
-#         ):
-#             try:
-#                 default_storage.delete(f.path)
-#             except:
-#                 pass
-
-# @receiver(post_delete, sender=sender)
 def post_save_image(sender, instance, *args, **kwargs):
     """ Clean Old Image file """
     try:
@@ -45,7 +13,6 @@ def post_save_image(sender, instance, *args, **kwargs):
     except:
         pass
 
-# @receiver(pre_save, sender=sender)
 def pre_save_image(sender, instance, *args, **kwargs):
     """ instance old image file will delete from os """
     try:
