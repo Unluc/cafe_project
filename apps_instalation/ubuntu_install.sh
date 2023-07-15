f838e8496ceabe03e4bd119693552241ea27b4a1
Unluc <padalka9619@gmail.com>
2023-07-15 16:16:11 +0100

    Fixed my script and added .envs(i know its not the best practice)

--- apps_instalation/ubuntu_install.sh
+++ apps_instalation/ubuntu_install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 apt-get update  # To get the latest package lists
 apt-get install python3 -y
+apt install docker.io
+apt install docker-compose
 apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
@@ -9,3 +11,5 @@ apt install make
 apt install build-essential
 apt install postgresql postgresql-contrib
 apt install postgresql postgresql-contrib
+cd ../frontend/
+npm install
